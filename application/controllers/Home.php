01c9de17eb1546c0c33cc17bfd23ba057f6f98dd
edersonlrf <edersonluis.rf@gmail.com>
2018-05-30 18:35:37 -0300

    autoloaders

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,8 +4,6 @@ class Home extends CI_Controller {
 
     public function index() {
 
-        $this->load->helper('url');
-
         $this->load->view('home/index');
 
     }
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title></title>
-    <link rel="stylesheet" href="<?php echo base_url("public/css/bootstrap.css"); ?>">
+    <link rel="stylesheet"
+        href="<?php echo base_url("public/css/bootstrap.css"); ?>">
 </head>
 <body>
     <h1>Teste</h1>
