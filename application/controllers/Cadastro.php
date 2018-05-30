ad275c70c0c09bceb7a20c9b9212a7eb8cba84a7
edersonlrf <edersonluis.rf@gmail.com>
2018-05-30 19:20:39 -0300

    form validation

--- application/controllers/Cadastro.php
+++ application/controllers/Cadastro.php
@@ -12,4 +12,25 @@ class Cadastro extends CI_Controller {
 
     }
 
+    public function novo() {
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('email','E-mail','required|min_length[4]|max_length[100]|is_unique[usuarios.email]');
+        $this->form_validation->set_rules('nome','Nome','required|min_length[4]|max_length[80]');
+        $this->form_validation->set_rules('senha','Senha','required|min_length[6]|max_length[100]');
+
+        if ($this->form_validation->run() == FALSE) {
+            $erros = array('msgs' => validation_errors());
+
+            $this->load->helper('form');
+
+            $this->load->view('partes/header');
+            $this->load->view('cadastro/index', $erros);
+            $this->load->view('partes/footer');
+        } else {
+            // ...
+        }
+
+    }
+
 }
