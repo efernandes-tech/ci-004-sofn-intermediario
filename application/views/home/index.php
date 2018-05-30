982391dd73fcef856816a1686465a00f2e95c8ad
edersonlrf <edersonluis.rf@gmail.com>
2018-05-30 18:27:07 -0300

    url helper

---
+++ application/views/home/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <link rel="stylesheet" href="<?php echo base_url("public/css/bootstrap.css"); ?>">
+</head>
+<body>
+    <h1>Teste</h1>
+</body>
+</html>
